83a846057a8276091ca284010822723ff6ea779e
MShafranova <mariia.shafranova@gmail.com>
2024-01-14 19:17:38 +0100

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector("form.login-form");
+loginForm.addEventListener("submit", formSubmit);
 
+function formSubmit(event) {
+
+  event.preventDefault();
+
+  const emailInput = loginForm.querySelector('input[name="email"]');
+  const passwordInput = loginForm.querySelector('input[name="password"]');
+
+  if (emailInput&&passwordInput) {
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+  
+  if (emailInput === "" || passwordInput === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const userInfo = {
+        email: emailValue,
+        password : passwordValue,
+    };
+    console.log(userInfo);
+  }
+  loginForm.reset();
+}
+}
