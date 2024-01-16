49042b66755a813224089b3b5da850a013475df8
MShafranova <mariia.shafranova@gmail.com>
2024-01-16 09:17:16 +0100

    task-1 fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
 const categories = document.querySelectorAll(".item");
-console.log(`Number of categories : ${categories.length}`);
-for (const category of categories) {
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach((category) => {
   const categoryTitle = category.querySelector("h2").textContent;
   const categoryElems = category.querySelectorAll("li");
-  console.log(`Category : ${categoryTitle}`);
-  console.log(`Elements : ${categoryElems.length}`);
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElems.length}`);
 }
+)
